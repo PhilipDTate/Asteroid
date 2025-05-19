0b11534ec51d70891aedecb39615525af5aaa798
ThePrimeagen <the.primeagen@aol.com>
2025-05-19 02:28:25 +0000

    D: worked on main.py and added .gitignore

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    
+    pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT), flags=0, depth=0, display=0, vsync=0).fill("black", rect=None, special_flags=0)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")    
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
