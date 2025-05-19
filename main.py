19a333e6ad4b4abf7c4e41075ca37753142fdde9
ThePrimeagen <the.primeagen@aol.com>
2025-05-19 02:38:29 +0000

    E: worked out some bugs within main.py

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ from constants import *
 def main():
     pygame.init()
     
-    pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT), flags=0, depth=0, display=0, vsync=0).fill("black", rect=None, special_flags=0)
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.display.flip()
     while True:
+        # This is to quit the game.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+
+        screen.fill("black", rect=None, special_flags=0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
