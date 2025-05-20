e021f4cd074cb75003aa12c290779c9eefd62730
ThePrimeagen <the.primeagen@aol.com>
2025-05-20 19:02:27 +0000

    T: Just editing a few minor things

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def main():
 
 
     pygame.display.flip()
+
+    # This is the game loop
     while True:
         # This is to quit the game.
         for event in pygame.event.get():
@@ -25,13 +27,13 @@ def main():
                 return
             
 
-        screen.fill("black", rect=None, special_flags=0)
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
 
         #limit to 60 fps
-        dt=clock.tick(60)/1000
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -5,9 +5,11 @@ class Player(CircleShape):
 
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation=0
+        self.rotation = 0
 
-    # in the player class
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -16,5 +18,3 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
