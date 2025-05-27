b9faa26fa5fa5d0264417a9413a76ac73865f7f1
ThePrimeagen <the.primeagen@aol.com>
2025-05-27 19:21:17 +0000

    K: Just fixing everything

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+import sys
 import pygame
 from constants import *
-from player import *
-from asteroid import *
-from asteroidfield import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
 
-
-    Player.containers = (updatable, drawable)
-
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
-    player=Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
-    asteroidField = AsteroidField()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
-    # This is the game loop
     while True:
-        # This is to quit the game.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
@@ -42,9 +43,10 @@ def main():
             obj.draw(screen)
 
         pygame.display.flip()
-        
-        #limit to 60 fps
-        dt = clock.tick(60)/1000
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
